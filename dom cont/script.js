68a8a6cdde860bb1e8794b170a5c288770a66c33
so0lo <dmi.sakovsky@gmail.com>
2021-08-05 21:27:22 +0300

    new com

--- dom cont/script.js
+++ dom cont/script.js
@@ -49,6 +49,68 @@ input2.addEventListener(`click`, function () {
 })
 
 
+const list2 = document.getElementById(`list2`);
+
+// list2.addEventListener(`click`, function (event) {
+//     //event.target.classList.remove(`test`);
+//     // add
+//     // remove
+//     // toggle добавляет класс и убирает
+//     //console.log(event.target.classList.contains(`test`));
+
+//     // event.target.style.color = 'red';
+//     // event.target.style.fontSize = `50px`;
+//     // event.target.style.border = `2px solid orange`;
+// });
+// list2.addEventListener(`mousemove`, function (event) {
+//     event.target.style.fontSize = `${event.clientY}px`;
+// });
 
 
+const chek = document.getElementById(`check`);
+const test = document.getElementById(`test`);
+
+// test.addEventListener(`click`, function () {
+//     // get
+//     // has
+//     // set
+//     console.log(check.setAttribute(`type`, `radio`));
+// });
+
+chek.addEventListener(`click`, function () {
+    console.log(check.checked);
+    test.disabled = !test.disabled;
+});
+
+const goods = [
+    {
+        name: `noski`,
+        price: 2
+    },
+    {
+        name: `trusy`,
+        price: 10
+    },
+    {
+        name: `shampoo`,
+        price: 5
+    },
+    {
+        name: `stanok`,
+        price: 100
+    }
+];
+
+function createElement (goodsName, price) {
+    const elem = document.createElement(`li`);
+    elem.textContent = goodsName;
+    elem.setAttribute(`data-price`, price);
+    return elem;
+}
+
+goods.forEach((tovar) => list2.append(createElement(tovar.name, tovar.price)));
+
+list2.addEventListener(`click`, function (event) {
+    console.log(event.target.dataset.price);
+});
 
